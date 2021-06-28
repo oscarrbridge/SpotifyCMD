034d6cc31ea0d702d12210735600d26b1ea432b4
Oscar Bridge <30382493+oscarrbridge@users.noreply.github.com>
2021-06-28 13:24:17 +1200

    Add files via upload

--- setup.py
+++ setup.py
@@ -8,15 +8,15 @@ installed_packages_list = sorted(["%s==%s" % (i.key, i.version)
                                   for i in installed_packages])
 
 new_list = []
-found_package = []
+found_package = False
 
 for package in range(len(installed_packages_list)):
     new_list.append(installed_packages_list[package].split("=="))
 
     if new_list[package][0] == "spotipy":
-        found_package.append(True)
+        found_package = True
 
-if found_package[0] is True:
+if found_package is True:
     print("Required packages found...")
 
 else:
